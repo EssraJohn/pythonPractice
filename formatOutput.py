364651cc3c0c705da50f1f5d38e16ee88c51e283
EssraJohn <essraj05@gmail.com>
2022-09-18 15:59:36 +0530

    Python

--- formatOutput.py
+++ formatOutput.py
@@ -4,8 +4,11 @@ name = input("My name is :")
 print(f"Hi {name}! It's a pleasure to see you again {name}")
 
 # Format as a Function
+# Initializing variables
 a = 5
 b = 23
+# addition
 sum = a + b
+# output
 print("The value of a is {} and b is {}".format(a, b))
 print("Sum {2},is the addition of {0} and {1}".format(a, b, sum))
