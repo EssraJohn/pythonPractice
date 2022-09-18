bcaf2b13335348b8d83e68c7e147ce7e3ec5486f
EssraJohn <essraj05@gmail.com>
2022-09-18 16:06:54 +0530

    Python

---
+++ dollarSignOutput.py
@@ -0,0 +1,14 @@
+# Using % Operator
+"""
+%d – integer
+%f – float
+%s – string
+%x – hexadecimal
+%o – octal
+"""
+name = input("Enter your first name:")
+age = int(input("What is your age:"))
+per = float(input("Enter total percentage:"))
+print("Your name is %s" % name)
+print("Your age is %d" % age)
+print("Your percentage is %f" % per)
