4bdc019481175f7b1253880cd203a584c47fc206
EssraJohn <essraj05@gmail.com>
2022-09-18 11:30:04 +0530

    Python

---
+++ multipleInputs.py
@@ -0,0 +1,5 @@
+# Multiple input
+a, b, c = map(int, input("Enter 3 integers:").split())
+print("The 3 integers you entered are:", end="")
+print(a, b, c)
+
---
+++ output1.py
@@ -0,0 +1,16 @@
+# Syntax: print(value(s), sep= ‘ ‘, end = ‘\n’, file=file, flush=flush)
+"""
+Parameters:
+value(s) : Any value, and as many as you like. Will be converted to string before printed
+sep=’separator’ : (Optional) Specify how to separate the objects, if there is more than one.Default :’ ‘
+end=’end’: (Optional) Specify what to print at the end.Default : ‘\n’
+file : (Optional) An object with a write method. Default :sys.stdout
+flush : (Optional) A Boolean, specifying if the output is flushed (True) or buffered (False). Default: False
+
+Returns: It returns output to the screen.
+
+"""
+print("CODE")
+print('C', 'O', 'D', 'E')
+print("Code", end="@")
+print("Pycharm", 'E', 'S', sep='#')
