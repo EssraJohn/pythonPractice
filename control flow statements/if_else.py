2fd66e51e7f5622a5e799b55ec9e7b3c5c5c2207
EssraJohn <essraj05@gmail.com>
2022-09-18 16:56:37 +0530

    Python

--- control flow statements/if_else.py
+++ control flow statements/if_else.py
@@ -1,5 +1,8 @@
+# if-else statement
 per = float(input("Enter your percentage:"))
+# if percentage is greater than and equal to 60
 if per >= 60:
     print("Your are eligible to apply")
+# if percentage is less than 60
 else:
     print("Sorry, you are not eligible")
