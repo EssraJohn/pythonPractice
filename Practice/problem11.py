cd34004862de7ff5e6e5c47a9d38a5e8ea43f769
EssraJohn <essraj05@gmail.com>
2023-06-19 18:10:07 +0530

    Python

--- Practice/problem11.py
+++ Practice/problem11.py
@@ -10,7 +10,7 @@ password = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ]
 # initializing an empty string
 otp = ""
 
-for letter in range((user_pass)):
+for letter in range(user_pass):
     guess_letter = password[randint(0, 9)]
     otp = str(guess_letter) + str(otp)
 
